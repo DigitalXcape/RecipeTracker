82f99026418bff54773a2de165b44fd076dce185
DigitalXcape <kodypweigel@gmail.com>
2024-09-24 16:06:05 -0500

    t

--- RecipeBook/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/RecipeBook.AssemblyInfo.cs
+++ RecipeBook/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/RecipeBook.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RecipeBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+4aaddfcca744cee003b9848deb1c53aa216b4a0d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+e72d9a118001e9026419b211cc5b38968b71a7f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("RecipeBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RecipeBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
